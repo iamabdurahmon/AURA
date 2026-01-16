83f90064776a9122d66cb4fd04ec33af88e62037
abduraxmon-dev <abdurahmonrahimovv@gmail.com>
2026-01-16 14:45:00 +0500

    build files and updates

--- server/index.ts
+++ server/index.ts
@@ -84,10 +84,9 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-const port = parseInt(process.env.PORT || "5000", 10);
-  
-  // Windows uchun moslashtirilgan listen funksiyasi
-  httpServer.listen(port, "127.0.0.1", () => {
-    log(`serving on port ${port}`);
-  });
+// 5000 ni 5001 ga almashtiring
+const port = parseInt(process.env.PORT || "5002", 10); 
+httpServer.listen(port, "0.0.0.0", () => {
+  log(`serving on port ${port}`);
+});
 })();
